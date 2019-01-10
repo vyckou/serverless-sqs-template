5dd743be949580d9f6f3361b2ae8ab6194519aa2
Vytautas Maciulskis <vyma@trustpilot.com>
2019-01-10 10:39:51 +0200

    initial commit

---
+++ lambda.py
@@ -0,0 +1,21 @@
+import json
+
+
+def handler(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+    # Use this code if you don't USE the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
